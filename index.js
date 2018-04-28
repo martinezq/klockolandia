5827336eb6c887c72f52096aef2c990052525cb3
Marcin Koch <marcin.koch@sap.com>
2018-04-28 08:37:17 +0200

    404 page + image optimization

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var static = require('node-static');
-var file = new static.Server('./build');
+var file = new static.Server('./build', { cache: 3600 });
 require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
+    
+    request.addListener('end', function() {
+        file.serve(request, response, function(e, res) {
+            if (e && (e.status === 404)) { // If the file wasn't found
+                file.serveFile('/404.html', 404, {}, request, response);
+            }
+        });
+    }).resume();
+
 }).listen(process.env.PORT || 3000);
